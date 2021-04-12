0cd0d36c24c975caf1419f983d6ee0f54d94d724
глеб соломин <glebsolomin@gmail.com>
2021-04-12 16:45:04 +0700

    Сделал функцию обновления базы данных 1. Функция доавляет записи в бд, беря данные из парсеров 2. Функция удаляет все прошлые записи

---
+++ alldata.py
@@ -0,0 +1,23 @@
+from db import init_db, get_list_of_films, add_film
+from Kinmomax_parser import parse as kinomaxparse
+from Goodwin import parseGoodwin
+from YouTube import findlink
+import sqlite3
+
+def update_data_base():
+    with sqlite3.connect('filmlist.db') as conn:
+        init_db(conn=conn, force=True)
+    kinomaxmas = kinomaxparse()
+    #goodwinmas = parseGoodwin()
+    for i in range(len(kinomaxmas)):
+        name = kinomaxmas[i]['title']
+        rating = kinomaxmas[i]['rating']
+        link = findlink(name)
+        with sqlite3.connect('filmlist.db') as conn:
+            add_film(conn=conn,
+                     cinema_id=1,
+                     name=name,
+                     rating=rating,
+                     link=link)
+
+update_data_base()
